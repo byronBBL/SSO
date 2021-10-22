5569c25584aaa967f9848ff3639625e826023003
byronBBL <13256001671@163.com>
2021-10-22 16:04:57 +0800

    第7次提交

---
+++ app1/src/main/java/cn/cqu/edu/HandlerInterceptor/LoginHandlerIntercep.java
@@ -0,0 +1,75 @@
+package cn.cqu.edu.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import org.apache.tomcat.util.http.parser.Cookie;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginHandlerIntercep implements HandlerInterceptor {
+
+    private static final String SECRET_KEY = "CQU_FOREVER&"; // 默认秘钥
+    /**
+     * 获得jwt中包含的信息,这里为用户的userid
+     * @param token jwt串
+     * @return userid,若为null表示jwt有问题或者超时
+     */
+    public static String getUserId(String token){
+        DecodedJWT jwt = null;
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET_KEY)).build();
+            jwt = verifier.verify(token);
+        } catch (Exception e) {
+            // e.printStackTrace(); 一般是jwt超时
+            return null;
+        }
+        Claim userId = jwt.getClaims().get("user_id");
+        if(userId != null){
+            return userId.asString();
+        } else return null;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // Object user = request.getSession().getAttribute("loginUser");
+        // if (user==null){
+        //     //未登录，返回登录页面
+        //     request.setAttribute("msg", "请先登录");
+        //     request.getRequestDispatcher("/").forward(request, response);
+        //     return false;
+        // }else {
+        //     //进行了登录
+        //     return true;
+        // }
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        javax.servlet.http.Cookie[] cookies = ((HttpServletRequest) request).getCookies();
+        String user_id = null;
+        if(cookies != null && cookies.length>0) {
+            for (javax.servlet.http.Cookie cookie : cookies) {
+                if("JWT".equals(cookie.getName()))
+                    user_id = getUserId(cookie.getValue());
+            }
+        }
+         // 获得网址的参数JWT
+         String JWT = httpRequest.getParameter("jwt_cookie");
+         System.out.println("jwt_cookie"+JWT);
+         if(JWT == null){  // 网址存在JWT，是cas已完成登录验证的返回
+            response.sendRedirect("http://localhost:8080/cas/login" + "?"
+            + "LOCAL_SERVICE" + "="
+            + httpRequest.getRequestURL());
+            return false;
+        }
+        else {  // 已有JWT，执行网页
+            request.setAttribute("user", user_id);
+            return true;
+        }
+    }
+    
+}
---
+++ app1/src/main/java/cn/cqu/edu/HandlerInterceptor/MyWebMvcConfig.java
@@ -0,0 +1,42 @@
+package cn.cqu.edu.HandlerInterceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// @Configuration
+// public class MyConfigurer implements WebMvcConfigurer {
+//     /**
+//      * 视图映射
+//      * @param registry
+//      */
+//     @Override
+//     public void addViewControllers(ViewControllerRegistry registry) {
+//         registry.addViewController("/").setViewName("login");
+//         registry.addViewController("/index").setViewName("login");
+//         registry.addViewController("/main.html").setViewName("redirect:students");
+//     }
+
+// }
+@Configuration
+public class MyWebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MyWebMvcConfig getMyWebMvcConfig(){
+        MyWebMvcConfig myWebMvcConfig = new MyWebMvcConfig() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("alogin");
+                registry.addViewController("/login").setViewName("alogin");
+                registry.addViewController("/main.html").setViewName("dashboard");
+            }
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new LoginHandlerIntercep()).addPathPatterns("/**")
+                        .excludePathPatterns("/login","/","/user/login");
+            }
+        };
+        return myWebMvcConfig;
+    }
